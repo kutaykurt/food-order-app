bac06c5cf6c204c645766e81a60c88f1b0e97e53
kutaykurt <Kutay_Kurt@gmx.de>
2023-03-22 18:41:39 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import { Fragment, useState } from 'react';
+import Cart from './components/Cart/Cart';
+import CardProvider from './store/CardProvider';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
+
+function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false)
+  }
+  return (
+    <CardProvider>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+      <main>
+        <Meals />
+      </main>
+    </CardProvider>
+  );
+}
+
+export default App;
